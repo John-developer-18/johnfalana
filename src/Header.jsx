02da64f34d7b96cc0cf6cf302731747fad879820
john <falanajohn6@gmail.com>
2023-10-10 15:38:58 +0100

    some commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,11 +2,14 @@ import {HiMenuAlt3} from 'react-icons/hi'
 import {RiMenu5Line} from 'react-icons/ri'
 import {useState, useEffect} from 'react'
 import {IoMdClose} from 'react-icons/io'
-import { useRef } from 'react'
+import Fade from 'react-reveal/Fade'
+
 function Header(){
-  const navRef = useRef();
-  const showNav = () => {
-    navRef.current.classList.toggle('block');
+ 
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleMenu = () =>{
+    setIsOpen((open) => !open )
   }
   const [showHeader, setShowHeader] = useState(true);
 
@@ -33,25 +36,35 @@ function Header(){
   const headerClasses = showHeader
   ? 'z-40 fixed top-0 left-0 right-0 w-full  mb-11 lg:hidden flex items-center md:justify-center justify-end px-5 py-5 visible transition-transform'
   : 'z-40 fixed top-0 left-0 right-0 w-full bg-black/5 backdrop-blur-lg mb-11 lg:hidden flex items-center md:justify-center justify-end px-5 py-3 visible transition-transform'  
+  
+  const menuStyles = isOpen 
+  ? "bg-myYellow top-0 bottom-0 left-32 right-0 fixed visble opacity-100 transition-opacity" 
+  : "bg-myYellow top-0 bottom-0 left-32 right-0 fixed hidden opacity-0 transition-opacity"
   return(
         <div className={headerClasses}>
-          <HiMenuAlt3 onClick={showNav}className='text-6xl font-bold text-myYellow md:hidden'/>
-          <RiMenu5Line onClick={showNav} className='text-6xl font-bold text-myYellow hidden md:block'/>
-          {/* <ul className='hidden md:flex justify-between w-1/2 text-myYellow text-2xl py-3'>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Skills</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul> */}
-          <div className='bg-myYellow top-0 bottom-0 left-32 right-0 fixed hidden'>
-          <div className='flex justify-end p-6'><IoMdClose className='text-6xl font-bold'/></div>
-          <div className='h-full grid place-content-center'>
-            <ul className='space-y-10 text-center'>
-              <li className='text-4xl font-bold'>About</li>
-              <li className='text-4xl font-bold'>Skills</li>
-              <li className='text-4xl font-bold'>Contact</li>
-            </ul>
-          </div>
-          </div>
+          <HiMenuAlt3 onClick={toggleMenu} className='text-6xl font-bold text-myYellow md:hidden'/>
+          <RiMenu5Line onClick={toggleMenu} className='text-6xl font-bold text-myYellow hidden md:block'/>
+          
+          {
+            isOpen?
+            (
+              <Fade>
+                <div className={menuStyles}>
+              <div className='flex justify-end p-6'><IoMdClose onClick={toggleMenu} className='text-6xl font-bold'/></div>
+              <div className='h-full grid place-content-center'>
+                <ul className='space-y-10 text-center'>
+                  <li className='text-4xl font-bold'>About</li>
+                  <li className='text-4xl font-bold'>Skills</li>
+                  <li className='text-4xl font-bold'>Contact</li>
+                </ul>
+              </div>
+              </div>
+              </Fade>
+            
+            ):
+            (<div></div>)
+          }
+
         </div>
         
     )
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -4,7 +4,9 @@ import Zoom from 'react-reveal/Zoom'
 function Skills() {
   return (
     <div className="py-20">
-     <h1 className="text-3xl text-center text-white font-semibold">Professional <span className="text-myYellow">Skillset</span></h1>
+      <Zoom>
+       <h1 className="text-3xl text-center text-white font-semibold">Professional <span className="text-myYellow">Skillset</span></h1>
+      </Zoom>
      <div className="min-h-screen px-20 md:px-10 grid grid-cols-1 md:grid-cols-4 gap-y-10 md:gap-y-1 gap-x-5 py-16">
         <Zoom>
         {
